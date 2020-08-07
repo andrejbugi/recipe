bfb1e239a4071c6c76a3a343ec28ea891e8480fe
andrejbugi <andrejbugi96@gmail.com>
2020-08-07 18:15:29 +0200

    add validations for meal_recipe model

--- app/models/meal_recipe.rb
+++ app/models/meal_recipe.rb
@@ -1,2 +1,11 @@
 class MealRecipe < ApplicationRecord
+  MAXIMUM_TITLE_LENGTH = 64
+  MAXIMUM_DESCRIPTION_LENGTH = 512
+  MINIMUM_DESCRIPTION_LENGTH = 32
+
+  validates :title, presence: true, length: { maximum: MAXIMUM_TITLE_LENGTH }
+  validates :description, presence: true, length: { minimum: MINIMUM_DESCRIPTION_LENGTH,
+                                                    maximum: MAXIMUM_DESCRIPTION_LENGTH }
+
+  belongs_to :user
 end
