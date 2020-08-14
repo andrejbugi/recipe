3c7f42a10825ce1b0f29e5f20db4578fed7491eb
andrejbugi <andrejbugi96@gmail.com>
2020-08-14 20:26:34 +0200

    Change Seeds.rb file for easy populating the DataBase

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create([{ username: 'Steve123', email: 'steve@gmail.com',
+               first_name: 'Steve', last_name: 'Rot',
+               password: '12341234', password_confirmation: '12341234' },
+             { username: 'Brad123', email: 'brad@gmail.com',
+                first_name: 'Brad', last_name: 'Pit',
+                password: '12341234', password_confirmation: '12341234' },
+             { username: 'John123', email: 'john@gmail.com',
+                first_name: 'John', last_name: 'Buschemi',
+                password: '12341234', password_confirmation: '12341234' }])
+
+User.first.meal_recipes.create({ title: 'First Recipe',
+                                 description: 'This first recipe is about making a first recipe' })
+
+User.second.meal_recipes.create({ title: 'Second Recipe',
+                                  description: 'This second recipe is about making a second recipe' })
+
+User.third.meal_recipes.create({ title: 'Last Recipe',
+                                 description: 'This second recipe is about making a second recipe' })
+
+MealRecipe.first.recipe_instructions.create({ instruction: "first make the instruction 1" })
+MealRecipe.first.recipe_instructions.create({ instruction: "second make the instruction 2" })
+
+MealRecipe.second.recipe_instructions.create({ instruction: "first make the instruction 1" })
+MealRecipe.second.recipe_instructions.create({ instruction: "second make the instruction 2" })
+
+MealRecipe.third.recipe_instructions.create({ instruction: "first make the instruction 1" })
+MealRecipe.third.recipe_instructions.create({ instruction: "second make the instruction 2" })
+
+MealRecipe.first.recipe_ingredients.create({ ingredient: "first make the ingredient 1" })
+MealRecipe.first.recipe_ingredients.create({ ingredient: "second make the ingredient 2" })
+
+MealRecipe.second.recipe_ingredients.create({ ingredient: "first make the ingredient 1" })
+MealRecipe.second.recipe_ingredients.create({ ingredient: "second make the ingredient 2" })
+
+MealRecipe.third.recipe_ingredients.create({ ingredient: "first make the ingredient 1" })
+MealRecipe.third.recipe_ingredients.create({ ingredient: "second make the ingredient 2" })
