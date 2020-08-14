bf80f5ea8513636f93c1fc219317b73bbffb1b1f
andrejbugi <andrejbugi96@gmail.com>
2020-08-14 17:59:47 +0200

    No blank ingredient/instruction, add Cancel Button in the forms

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,3 +1,5 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :meal_recipe
+
+  validates :ingredient, presence: true
 end
--- app/models/recipe_instruction.rb
+++ app/models/recipe_instruction.rb
@@ -1,3 +1,5 @@
 class RecipeInstruction < ApplicationRecord
   belongs_to :meal_recipe
+
+  validates :instruction, presence: true
 end
